0f294825d1b30150ac6abd6267ab8af564844b4e
Luan Louzada <luanlouzadadev@gmail.com>
2025-03-16 21:30:31 -0300

    fix: quick fix hardcoded america sao paulo

--- src/main/java/br/com/gerenciador/GerenciadorApplication.java
+++ src/main/java/br/com/gerenciador/GerenciadorApplication.java
@@ -1,8 +1,12 @@
 package br.com.gerenciador;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jakarta.annotation.PostConstruct;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +14,11 @@ public class GerenciadorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GerenciadorApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
     }
 
 }
